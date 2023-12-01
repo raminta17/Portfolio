22ad4282aabd80736c9d1b3b289a65586cf6a7a2
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-12-01 15:33:30 +0200

    headings changed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,69 +1,77 @@
-import {useEffect, useState} from "react";
-import {motion} from "framer-motion";
-import { useInView } from 'react-intersection-observer';
-import {useUserStore} from "../modules/store.tsx";
-import {ShowModalType} from "../modules/types.tsx";
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useUserStore } from '../modules/store.tsx'
+import { ShowModalType } from '../modules/types.tsx'
 
+const Header = ({ setShowModal }: ShowModalType) => {
+	const [showArrow, setShowArrow] = useState(true)
+	const { ref: homeRef, inView: isHomeInView } = useInView({ threshold: 0.5 })
+	const {
+		setIsHomeInView,
+		setIsAboutInView,
+		setIsSkillsInView,
+		setIsEducationInView,
+		setIsProjectsInView,
+		setIsContactsInView,
+	} = useUserStore((state) => ({
+		setIsHomeInView: state.setIsHomeInView,
+		setIsProjectsInView: state.setIsProjectsInView,
+		setIsSkillsInView: state.setIsSkillsInView,
+		setIsEducationInView: state.setIsEducationInView,
+		setIsContactsInView: state.setIsContactsInView,
+		setIsAboutInView: state.setIsAboutInView,
+	}))
 
-const Header = ({setShowModal}:ShowModalType) => {
+	useEffect(() => {
+		if (isHomeInView) {
+			setIsProjectsInView(false)
+			setIsContactsInView(false)
+			setIsSkillsInView(false)
+			setIsEducationInView(false)
+			setIsAboutInView(false)
+			setIsHomeInView(true)
+		}
+	}, [isHomeInView])
 
-    const [showArrow, setShowArrow] = useState(true);
-    const {ref: homeRef, inView: isHomeInView} = useInView({threshold:0.5});
-    const {setIsHomeInView, setIsAboutInView, setIsSkillsInView, setIsEducationInView, setIsProjectsInView, setIsContactsInView} = useUserStore((state) => ({
-        setIsHomeInView: state.setIsHomeInView,
-        setIsProjectsInView: state.setIsProjectsInView,
-        setIsSkillsInView: state.setIsSkillsInView,
-        setIsEducationInView: state.setIsEducationInView,
-        setIsContactsInView: state.setIsContactsInView,
-        setIsAboutInView: state.setIsAboutInView
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 10) {
+				setShowArrow(false)
+			} else {
+				setShowArrow(true)
+			}
+		})
+	}, [])
 
-    }) )
+	return (
+		<div
+			id='Home'
+			className='parallaxBg header centered text-center flex-column gap-5'
+			ref={homeRef}
+		>
+			<div className='d-flex justify-content-center align-items-center flex-column gap-2'>
+				<div className='heading1'>Hi there, I'm Raminta</div>
+				<div className='heading2'>Full Stack Developer</div>
+				<div className='heading5'>Welcome to my Portfolio</div>
+				<motion.div
+					onClick={() => setShowModal(true)}
+					className='getInTouchBtn d-flex align-items-center justify-content-center'
+					whileHover={{ scale: 1.1 }}
+					transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+				>
+					GET IN TOUCH
+				</motion.div>
+			</div>
+			{showArrow && (
+				<svg className='arrows'>
+					<path className='a1' d='M0 0 L30 32 L60 0'></path>
+					<path className='a2' d='M0 20 L30 52 L60 20'></path>
+					<path className='a3' d='M0 40 L30 72 L60 40'></path>
+				</svg>
+			)}
+		</div>
+	)
+}
 
-    useEffect(() => {
-        if (isHomeInView) {
-            setIsProjectsInView(false)
-            setIsContactsInView(false)
-            setIsSkillsInView(false)
-            setIsEducationInView(false)
-            setIsAboutInView(false)
-            setIsHomeInView(true)
-        }
-    }, [isHomeInView])
-
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 10) {
-                setShowArrow(false);
-            } else {
-                setShowArrow(true);
-            }
-        });
-    }, []);
-
-    return (
-        <div id="Home" className="parallaxBg header centered text-center flex-column gap-5" ref={homeRef}>
-            <div className="d-flex justify-content-center align-items-center flex-column gap-2">
-                <h1>Hi there, I'm Raminta</h1>
-                <h2>Full Stack Developer</h2>
-                <h5>Welcome to my Portfolio</h5>
-                <motion.div
-                    onClick={() => setShowModal(true)}
-                    className="getInTouchBtn d-flex align-items-center justify-content-center"
-                    whileHover={{ scale: 1.1 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                >
-                    GET IN TOUCH
-                </motion.div>
-            </div>
-            {showArrow &&
-                <svg className="arrows">
-                    <path className="a1" d="M0 0 L30 32 L60 0"></path>
-                    <path className="a2" d="M0 20 L30 52 L60 20"></path>
-                    <path className="a3" d="M0 40 L30 72 L60 40"></path>
-                </svg>
-            }
-            </div>
-    );
-};
-
-export default Header;
+export default Header
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,68 +1,127 @@
-import {useEffect, useRef, useState} from "react";
-import {useUserStore} from "../modules/store.tsx";
+import { useEffect, useRef, useState } from 'react'
+import { useUserStore } from '../modules/store.tsx'
 
 const Navigation = () => {
+	const [navClassNames, setNavClassNames] = useState('nav')
+	const [fixed, setFixed] = useState('')
+	const navRef = useRef<HTMLElement | null>(null)
+	const {
+		isHomeInView,
+		isAboutInView,
+		isSkillsInView,
+		isEducationInView,
+		isProjectsInView,
+		isContactsInView,
+	} = useUserStore((state) => ({
+		isAboutInView: state.isAboutInView,
+		isHomeInView: state.isHomeInView,
+		isSkillsInView: state.isSkillsInView,
+		isEducationInView: state.isEducationInView,
+		isProjectsInView: state.isProjectsInView,
+		isContactsInView: state.isContactsInView,
+	}))
 
-    const [navClassNames, setNavClassNames] = useState('nav');
-    const [fixed, setFixed] = useState('');
-    const navRef = useRef<HTMLElement | null>(null);
-    const {isHomeInView, isAboutInView, isSkillsInView, isEducationInView, isProjectsInView, isContactsInView} = useUserStore(state => ({
-        isAboutInView: state.isAboutInView,
-        isHomeInView: state.isHomeInView,
-        isSkillsInView: state.isSkillsInView,
-        isEducationInView: state.isEducationInView,
-        isProjectsInView : state.isProjectsInView,
-        isContactsInView: state.isContactsInView
-    }));
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			if (window.scrollY > 0) {
+				setFixed('fixed')
+			} else {
+				setFixed('')
+			}
+		})
+	}, [])
+	function handleResponsiveMenu() {
+		if (navClassNames === 'nav') {
+			setNavClassNames('nav responsive')
+		} else {
+			setNavClassNames('nav')
+		}
+	}
 
-    useEffect(() => {
-        window.addEventListener('scroll', () => {
-            if (window.scrollY > 0) {
-                setFixed('fixed');
-            } else {
-                setFixed('');
-            }
-        });
-    }, []);
-    function handleResponsiveMenu() {
-        if (navClassNames === 'nav') {
-            setNavClassNames('nav responsive');
-        } else {
-            setNavClassNames('nav');
-        }
-    }
+	return (
+		<nav
+			className={`align-items-center nav sm-column ${navClassNames}`}
+			ref={navRef}
+			id={fixed}
+		>
+			<div className='burgerMenu p-3' onClick={handleResponsiveMenu}>
+				<i className='fa-solid fa-bars'></i>
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					onClick={() => window.scroll({ behavior: 'smooth' })}
+					className={isHomeInView ? 'largeNavText' : 'smallNavText'}
+					href='#Home'
+				>
+					Home
+				</a>
+				<hr />
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					className={isAboutInView ? 'largeNavText' : 'smallNavText'}
+					href='#About'
+				>
+					About
+				</a>
+				<hr />
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					className={isSkillsInView ? 'largeNavText' : 'smallNavText'}
+					href='#Skills'
+				>
+					Skills
+				</a>
+				<hr />
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					className={isEducationInView ? 'largeNavText' : 'smallNavText'}
+					href='#Education'
+				>
+					Education
+				</a>
+				<hr />
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					className={isProjectsInView ? 'largeNavText' : 'smallNavText'}
+					href='#Projects'
+				>
+					Projects
+				</a>
+				<hr />
+			</div>
+			<div
+				className='navDiv text-center'
+				onClick={() => setNavClassNames('nav')}
+			>
+				<a
+					className={isContactsInView ? 'largeNavText' : 'smallNavText'}
+					href='#Contacts'
+				>
+					Contacts
+				</a>
+				<hr />
+			</div>
+		</nav>
+	)
+}
 
-    return (
-        <nav className={`align-items-center nav sm-column ${navClassNames}`} ref={navRef} id={fixed}>
-            <div className="burgerMenu p-3" onClick={handleResponsiveMenu}>
-                <i className="fa-solid fa-bars"></i>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a onClick={() => window.scroll({behavior:"smooth"})} className={isHomeInView ? 'largeNavText' : 'smallNavText'} href="#Home">Home</a>
-                <hr/>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a className={isAboutInView ? 'largeNavText' : 'smallNavText'} href="#About">About</a>
-                <hr/>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a className={isSkillsInView ? 'largeNavText' : 'smallNavText'} href="#Skills">Skills</a>
-                <hr/>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a className={isEducationInView ? 'largeNavText' : 'smallNavText'} href="#Education">Education</a>
-                <hr/>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a className={isProjectsInView ? 'largeNavText' : 'smallNavText'} href="#Projects">Projects</a>
-                <hr/>
-            </div>
-            <div className="navDiv text-center" onClick={()=> setNavClassNames('nav')}>
-                <a className={isContactsInView ? 'largeNavText' : 'smallNavText'}  href="#Contacts">Contacts</a>
-                <hr/>
-            </div>
-        </nav>
-    );
-};
-
-export default Navigation;
+export default Navigation
