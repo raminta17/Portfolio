2b59433e00030a8daf52cf3962170c9f564376c4
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-12-01 11:36:10 +0200

    vite config env bug update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	define: {
+		VITE_API_BASE_URL: process.env.VITE_API_BASE_URL,
+	},
 })
